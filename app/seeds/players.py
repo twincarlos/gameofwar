08b2fbfcc2f77375f314bcac2b712377e9cfae91
carlos rodriguez <twincarlos98@gmail.com>
2022-07-11 22:50:58 -0600

    fixed seeders

--- app/seeds/players.py
+++ app/seeds/players.py
@@ -3,9 +3,11 @@ from app.models import db, Player
 def seed_players():
     player_1 = Player()
     player_2 = Player()
+    no_player = Player()
 
     db.session.add(player_1)
     db.session.add(player_2)
+    db.session.add(no_player)
     
     db.session.commit()
 
